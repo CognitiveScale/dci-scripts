2bc34e1f47d1c50d8261241b3f50da03dc5f1d7d
pmantaring-cs <46978495+pmantaring-cs@users.noreply.github.com>
2019-08-28 10:52:02 -0500

    Update create-token-actions.sh

--- create-token-actions.sh
+++ create-token-actions.sh
@@ -7,6 +7,7 @@ IFS=$'\n'
 #sleep 10 # TODO make this less static and perform status check w/ backoff
 podID=$(kubectl get pods -n cortex -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep 'cortex-accounts')
 acct_jwt_god_token=$(kubectl exec -n cortex -i "${podID}" -- bash -c "node /app/generate-god-jwt-for-docker.js")
+echo "JWT god token=$acct_jwt_god_token"
 # obtain the list of action containers
 actionString=$(kubectl get pods -n cortex -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep 'cortex-actions')
 # convert new line to arrays
@@ -27,11 +28,13 @@ for ((i = 0; i < ${#actionList[@]}; i++)); do
 
     if [ $i -eq 0 ]; then
         # update secrets for k8s
+        echo "updating secrets for k8s"
         docker_secrets_config_actions=$(kubectl exec -i -n cortex "${actionList[$i]}" -- bash -c 'cat ~/.docker/config.json')
         docker_secrets_config_actions_no_newline=$(echo -n "$docker_secrets_config_actions" | jq -c '.')
         docker_secrets_config_base64=$(echo -n "$docker_secrets_config_actions_no_newline" | base64)
         docker_secrets_config_actions_no_newline_escaped=$(echo "$docker_secrets_config_actions_no_newline" | sed -E 's/([(){}"/:.]){1}/\\\1/g')
         kubectl get secret -n cortex docker-login -o json | jq --arg updated_value "$docker_secrets_config_base64" '.data[".dockerconfigjson"]=$updated_value' | kubectl apply -f -
         kubectl get secret -n cortex-compute docker-login -o json | jq --arg updated_value "$docker_secrets_config_base64" '.data[".dockerconfigjson"]=$updated_value' | kubectl apply -f -
+        echo "done updating secrets for k8s"
     fi
 done
